f5e24507caa8f5ff5afda0e71d8a2cbe1c3e7f20
yuvrajgrover6 <yuvrajgrover6@gmail.com>
2024-11-17 17:32:17 -0500

    Refactor user_routes to include userId parameter in upload-profile-pic route

--- src/modules/user/route/user_routes.ts
+++ src/modules/user/route/user_routes.ts
@@ -5,7 +5,7 @@ import get_profile_image_controller from "../controller/get_profile_image_servic
 const router = Router();
 
 router.post("/user-details", get_user_details_controller);
-router.post("/upload-profile-pic", upload_profile_pic_controller);
+router.post("/upload-profile-pic/:userId", upload_profile_pic_controller);
 router.get("/download", get_profile_image_controller);
 
 export default router;
