f29c5ce5dbedf698f08d7f656d43e18053e33c1e
yuvrajgrover6 <yuvrajgrover6@gmail.com>
2024-11-17 17:30:35 -0500

    Refactor user_routes to include upload-profile-pic and download routes

--- src/modules/user/route/user_routes.ts
+++ src/modules/user/route/user_routes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import get_user_details_controller from "../controller/get_user_details_controller";
+import upload_profile_pic_controller from "../controller/update_profilePic_controller";
+import get_profile_image_controller from "../controller/get_profile_image_service";
 const router = Router();
 
 router.post("/user-details", get_user_details_controller);
+router.post("/upload-profile-pic", upload_profile_pic_controller);
+router.get("/download", get_profile_image_controller);
 
 export default router;
